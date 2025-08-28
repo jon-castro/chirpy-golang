f02cca29398af8a32b7eb3324cbd0082c904c237
jon-castro <jcastrogarcia@gmail.com>
2025-08-28 02:41:11 -0500

    Work on server with HandlerFunc

--- main.go
+++ main.go
@@ -4,20 +4,25 @@ import "net/http"
 
 func main() {
 
-import "net/http"
-
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/assets", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handler)
 
 	err := server.ListenAndServe()
 	if err != nil {
 		return
 	}
 
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
